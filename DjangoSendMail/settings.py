3cd08ba7f26cf8c0adbb599dc05b39be70399921
Ahmad Rosid K <ahmadrosid30121997@gmail.com>
2020-05-02 08:16:39 +0700

    Setup templates

--- DjangoSendMail/settings.py
+++ DjangoSendMail/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'DjangoSendMail.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
