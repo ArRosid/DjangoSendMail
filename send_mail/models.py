18dc654b80d140a306f73772eddeae29cff0872a
Ahmad Rosid K <ahmadrosid30121997@gmail.com>
2020-05-02 07:53:14 +0700

    Add EmailList models

--- send_mail/models.py
+++ send_mail/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class EmailList(models.Model):
+    email = models.EmailField(max_length=100)
+    send = models.BooleanField(default=False)
+    last_send = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
