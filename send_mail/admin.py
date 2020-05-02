3f87a9ce4939376c8cdf594d11e4e972e9c63359
Ahmad Rosid K <ahmadrosid30121997@gmail.com>
2020-05-02 10:07:02 +0700

    modified admin add fields

--- send_mail/admin.py
+++ send_mail/admin.py
@@ -5,6 +5,7 @@ from . import models
 
 class EmailListAdmin(admin.ModelAdmin):
     list_display = ('email', 'send', 'last_send')
+    fields = ('email',)
 
 
 admin.site.register(models.EmailList, EmailListAdmin)
