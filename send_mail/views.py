6645886f49a754244484b931a343d85ce73b466c
Ahmad Rosid K <ahmadrosid30121997@gmail.com>
2020-05-02 08:26:08 +0700

    Add datatables in home page to display email list

--- send_mail/views.py
+++ send_mail/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from . import models
+
 
 def home(request):
-    return render(request, "send_mail/home.html")
+    email_list = models.EmailList.objects.all()
+    context = {
+        'email_list': email_list
+    }
+    return render(request, "send_mail/home.html", context)
